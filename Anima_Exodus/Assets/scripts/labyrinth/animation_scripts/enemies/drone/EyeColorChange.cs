6e2d1033edb10d927b7a604a756fe95e9e789c4a
ArcadeRoyal <37913846+ArcadeRoyal@users.noreply.github.com>
2018-04-06 17:29:40 -0500

    Addt'l animations
    Implemented Enemy > Player attack animations, added color tuning

---
+++ Anima_Exodus/Assets/scripts/labyrinth/animation_scripts/enemies/drone/EyeColorChange.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Labyrinth.Animations
+{
+    public class EyeColorChange : MonoBehaviour
+    {
+
+        public int mat_layer;
+        public Color baseColor;
+        public Color attackColor; 
+
+        private Renderer meshRenderer;
+        private Color currentColor; 
+
+        // Use this for initialization
+        void Awake()
+        {
+
+            meshRenderer = GetComponentInChildren<Renderer>(); 
+
+        }
+
+        private void Start()
+        {
+            currentColor = baseColor; 
+        }
+
+        private void Update()
+        {
+
+            float emission = (Mathf.PingPong(Time.time / 4, .5f));
+            meshRenderer.materials[mat_layer].color = currentColor;
+            Color lightColor = currentColor * Mathf.LinearToGammaSpace(emission);
+            meshRenderer.materials[mat_layer].SetColor("_EmissionColor", lightColor); 
+
+
+        }
+
+        public void StartAttack( float lerpTime )
+        {
+            StartCoroutine(Attack(lerpTime)); 
+        }
+
+        public IEnumerator Attack( float lerpTime )
+        {
+            float elapsedTime = 0; 
+            while (elapsedTime < lerpTime)
+            {
+                elapsedTime += Time.deltaTime;
+                currentColor = Color.Lerp(baseColor, attackColor, elapsedTime / lerpTime);
+                yield return null; 
+            }
+            currentColor = attackColor; 
+        }
+
+        public void EndAttack(float lerpTime)
+        {
+            StartCoroutine(StopAttack(lerpTime));
+        }
+
+        public IEnumerator StopAttack(float lerpTime)
+        {
+            float elapsedTime = 0;
+            while (elapsedTime < lerpTime)
+            {
+                elapsedTime += Time.deltaTime;
+                currentColor = Color.Lerp(attackColor, baseColor, elapsedTime / lerpTime);
+                yield return null;
+            }
+            currentColor = baseColor;
+        }
+
+    }
+}
+
+
